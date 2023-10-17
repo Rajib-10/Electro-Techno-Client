27227155cbf2982e2d901f2b5f3755e8f843cd44
Rajib <rajib@ubuntu>
2023-10-18 03:09:28 +0600

    Environment setup using react router,tailwind and daiusy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold text-center">Hello Electro & Techno</h1>
+     
+    </>
+  )
+}
+
+export default App
