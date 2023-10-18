9d567ddd71daac17ffa48375f034b2dd7094d277
Rajib <rajib@ubuntu>
2023-10-18 23:05:25 +0600

    login page content added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,57 @@
+/* eslint-disable react/no-unknown-property */
 
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+
+    const handleSubmit =e=>{
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        const user = {email,password}
+        console.log(user)
+        
+    }
+
+    const handleGoogle=()=>{
+        console.log("google icon is clicking")
+    }
+
     return (
-        <div>
-            this is login page
-        </div>
+      <div className="flex justify-center items-center my-10">
+          <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-700 ">
+	<h1 className="text-2xl font-bold text-center text-white">Login</h1>
+	<form  className="space-y-6" onSubmit={handleSubmit}>
+		<div className="space-y-1 text-sm">
+			<label for="username" className="block dark:text-gray-400">Email</label>
+			<input type="email" name="email" id="email" placeholder="Your Email" className="w-full px-4 py-3 rounded-md outline-none" />
+		</div>
+		<div className="space-y-1 text-sm">
+			<label for="password" className="block dark:text-gray-400">Password</label>
+			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md outline-none " />
+			
+		</div>
+		<input type="submit" value="Sign IN" className="block w-full cursor-pointer p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400"/>
+	</form>
+	<div className="flex items-center pt-4 space-x-1">
+		<div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+		<p className="px-3 text-sm dark:text-gray-400">Login with social accounts</p>
+		<div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+	</div>
+	<div className="flex justify-center space-x-4">
+		<button onClick={handleGoogle} className="p-3 rounded-sm">
+			<FcGoogle className="text-2xl" />
+		</button>
+		
+		
+	</div>
+	<p className="text-xs text-center sm:px-6 dark:text-gray-400">Don not have an account?
+		<Link to='/register'  className="underline dark:text-gray-100">Sign up</Link>
+	</p>
+</div>
+      </div>
     );
 };
 
